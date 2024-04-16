25fa6b43bfc52067def48bcf5035270efb378910
Alexey_Dzhevello <fishwere@mail.ru>
2024-04-16 19:08:51 +0500

    task 1 almost complete

--- index.js
+++ index.js
@@ -9,13 +9,54 @@ submitButton.addEventListener('click', (event) => {
 const closeModalButton = document.querySelector('.accepted-modal .close');
 closeModalButton.addEventListener('click', () => acceptedModal.close());
 
+
+function addCardDeleteButtonListener(card){
+    card
+        .querySelector('.remove-button')
+        .addEventListener('click', (e) => {
+                if (document.querySelectorAll('.beverage').length <= 1){
+                    return;
+                }
+                e.target.closest('.beverage').remove();
+                restoreBeverageCardNumeration();
+            }
+        );
+}
+
 document
     .querySelector('.add-button')
     .addEventListener('click', () => addBeverageCard());
 
 
+addCardDeleteButtonListener(document.querySelector('.beverage'));
+restoreBeverageCardNumeration();
+
+
 function addBeverageCard() {
     const newCard = document.querySelector('.beverage').cloneNode(deep=true);
+    setRadioInputNameFor(newCard, 'abra kadabra');
+    addCardDeleteButtonListener(newCard);
     const form = document.querySelector('form');
     form.insertBefore(newCard, form.children[form.childElementCount-2]);
+    restoreBeverageCardNumeration();
+}
+
+
+function restoreBeverageCardNumeration() {
+    let nextNumber = 1;
+    document
+        .querySelectorAll('.beverage')
+        .forEach(
+            (el) => {
+                el.querySelector('.beverage-count').textContent = `Напиток №${nextNumber}`;
+                setRadioInputNameFor(el, `milkForBeverage${nextNumber}`);
+                nextNumber++;
+            }
+        );
+}
+
+
+function setRadioInputNameFor(card, name){
+    card.querySelectorAll("input[type='radio']")
+        .forEach((el) => el.setAttribute('name', name));
 }
